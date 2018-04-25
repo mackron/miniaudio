d977266bf331cb007260e8920ce23e6ed3ba5184
David Reid <mackron@gmail.com>
2018-04-25 10:34:41 +1000

    Minor change to the Windows build script for test_0.

--- tests/mal_test_0_build.bat
+++ tests/mal_test_0_build.bat
@@ -1,2 +1,2 @@
-gcc mal_test_0.c   -o bin/mal_test_0.exe     -Wall -mavx
-g++ mal_test_0.cpp -o bin/mal_test_0_cpp.exe -Wall -mavx
+gcc mal_test_0.c   -o ./bin/mal_test_0.exe     -Wall -mavx
+g++ mal_test_0.cpp -o ./bin/mal_test_0_cpp.exe -Wall -mavx
