9e14a9f8f8fec249146dd9a325873ac9dba127c5
David Reid <mackron@gmail.com>
2018-04-29 16:42:47 +1000

    Update tests.

--- tests/mal_test_0_build.bat
+++ tests/mal_test_0_build.bat
@@ -1,7 +1,14 @@
 @echo off
 SET c_compiler=gcc
 SET cpp_compiler=g++
+SET options=-Wall -mavx
 @echo on
 
-%c_compiler% mal_test_0.c   -o ./bin/mal_test_0.exe     -Wall -mavx
-%cpp_compiler% mal_test_0.cpp -o ./bin/mal_test_0_cpp.exe -Wall -mavx
+%c_compiler% mal_test_0.c    -o ./bin/mal_test_0.exe        %options%
+%cpp_compiler% mal_test_0.cpp  -o ./bin/mal_test_0_cpp.exe    %options%
+
+%c_compiler% mal_profiling.c -o ./bin/mal_profiling.exe     %options%
+%cpp_compiler% mal_profiling.c -o ./bin/mal_profiling_cpp.exe %options%
+
+%c_compiler% mal_dithering.c -o ./bin/mal_dithering.exe     %options%
+%cpp_compiler% mal_dithering.c -o ./bin/mal_dithering_cpp.exe %options%
