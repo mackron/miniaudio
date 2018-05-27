dba808162da685c277908d0b7c1417bb7b7e18a7
David Reid <mackron@gmail.com>
2018-05-27 21:10:03 +1000

    Update test build script to use AVX2 instead of AVX.

--- tests/mal_build_tests_win32.bat
+++ tests/mal_build_tests_win32.bat
@@ -1,14 +1,14 @@
 @echo off
 SET c_compiler=gcc
 SET cpp_compiler=g++
-SET options=-Wall -mavx
+SET options=-Wall -mavx2
 @echo on
 
 %c_compiler% mal_test_0.c    -o ./bin/mal_test_0.exe        %options%
 %cpp_compiler% mal_test_0.cpp  -o ./bin/mal_test_0_cpp.exe    %options%
 
-%c_compiler% mal_profiling.c -o ./bin/mal_profiling.exe     %options%
-%cpp_compiler% mal_profiling.c -o ./bin/mal_profiling_cpp.exe %options%
+%c_compiler% mal_profiling.c -o ./bin/mal_profiling.exe     %options% -s -O2
+%cpp_compiler% mal_profiling.c -o ./bin/mal_profiling_cpp.exe %options% -s -O2
 
 %c_compiler% mal_dithering.c -o ./bin/mal_dithering.exe     %options%
 %cpp_compiler% mal_dithering.c -o ./bin/mal_dithering_cpp.exe %options%
