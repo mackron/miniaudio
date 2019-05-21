8a6800493b830b8926ce341235f85f01b932224f
David Reid <mackron@gmail.com>
2019-05-21 17:39:29 +1000

    Update revision history.

--- miniaudio.h
+++ miniaudio.h
@@ -1,6 +1,6 @@
 /*
 Audio playback and capture library. Choice of public domain or MIT-0. See license statements at the end of this file.
-miniaudio (formerly mini_al) - v0.9.5 - 2019-xx-xx
+miniaudio (formerly mini_al) - v0.9.5 - 2019-05-21
 
 David Reid - davidreidsoftware@gmail.com
 */
@@ -33035,10 +33035,11 @@ Device
 /*
 REVISION HISTORY
 ================
-v0.9.5 - 2019-xx-xx
+v0.9.5 - 2019-05-21
   - Add logging to ma_dlopen() and ma_dlsym().
   - Add ma_decoder_get_length_in_pcm_frames().
   - Fix a bug with capture on the OpenSL|ES backend.
+  - Fix a bug with the ALSA backend where a device would not restart after being stopped.
 
 v0.9.4 - 2019-05-06
   - Add support for C89. With this change, miniaudio should compile clean with GCC/Clang with "-std=c89 -ansi -pedantic" and
