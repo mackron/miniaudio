31f45081d0c33d4935bf70d81548f1d98deb1932
David Reid <mackron@gmail.com>
2020-02-02 13:19:39 +1000

    Update emscripten tests build script.

--- tests/ma_build_tests_emscripten.bat
+++ tests/ma_build_tests_emscripten.bat
@@ -1,2 +1,2 @@
-emcc ./ma_test_0.c -o ./bin/ma_test_0_emscripten.html -s WASM=0 -std=c99
-emcc ./ma_duplex.c -o ./bin/ma_duplex.html            -s WASM=0 -std=c99
+::emcc ./ma_test_0.c -o ./bin/ma_test_0_emscripten.html -s WASM=0 -Wall
+emcc ./ma_duplex.c -o ./bin/ma_duplex.html            -s WASM=0 -Wall
