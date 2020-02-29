3cb9622a96d0a15692e381111ddf1977d39d41c9
David Reid <mackron@gmail.com>
2020-02-29 11:59:11 +1000

    Add more results codes in preparation for future work.

--- miniaudio.h
+++ miniaudio.h
@@ -1657,15 +1657,59 @@ typedef ma_uint8 ma_channel;
 
 typedef int ma_result;
 #define MA_SUCCESS                                      0
-
-/* General errors. */
-#define MA_ERROR                                       -1      /* A generic error. */
+#define MA_ERROR                                       -1   /* A generic error. */
 #define MA_INVALID_ARGS                                -2
 #define MA_INVALID_OPERATION                           -3
 #define MA_OUT_OF_MEMORY                               -4
-#define MA_ACCESS_DENIED                               -5
-#define MA_TOO_LARGE                                   -6
-#define MA_TIMEOUT                                     -7
+#define MA_OUT_OF_RANGE                                -5
+#define MA_ACCESS_DENIED                               -6
+#define MA_DOES_NOT_EXIST                              -7
+#define MA_ALREADY_EXISTS                              -8
+#define MA_TOO_MANY_OPEN_FILES                         -9
+#define MA_INVALID_FILE                                -10
+#define MA_TOO_BIG                                     -11
+#define MA_PATH_TOO_LONG                               -12
+#define MA_NAME_TOO_LONG                               -13
+#define MA_NOT_DIRECTORY                               -14
+#define MA_IS_DIRECTORY                                -15
+#define MA_DIRECTORY_NOT_EMPTY                         -16
+#define MA_END_OF_FILE                                 -17
+#define MA_NO_SPACE                                    -18
+#define MA_BUSY                                        -19
+#define MA_IO_ERROR                                    -20
+#define MA_INTERRUPT                                   -21
+#define MA_UNAVAILABLE                                 -22
+#define MA_ALREADY_IN_USE                              -23
+#define MA_BAD_ADDRESS                                 -24
+#define MA_BAD_SEEK                                    -25
+#define MA_BAD_PIPE                                    -26
+#define MA_DEADLOCK                                    -27
+#define MA_TOO_MANY_LINKS                              -28
+#define MA_NOT_IMPLEMENTED                             -29
+#define MA_NO_MESSAGE                                  -30
+#define MA_BAD_MESSAGE                                 -31
+#define MA_NO_DATA_AVAILABLE                           -32
+#define MA_INVALID_DATA                                -33
+#define MA_TIMEOUT                                     -34
+#define MA_NO_NETWORK                                  -35
+#define MA_NOT_UNIQUE                                  -36
+#define MA_NOT_SOCKET                                  -37
+#define MA_NO_ADDRESS                                  -38
+#define MA_BAD_PROTOCOL                                -39
+#define MA_PROTOCOL_UNAVAILABLE                        -40
+#define MA_PROTOCOL_NOT_SUPPORTED                      -41
+#define MA_PROTOCOL_FAMILY_NOT_SUPPORTED               -42
+#define MA_ADDRESS_FAMILY_NOT_SUPPORTED                -43
+#define MA_SOCKET_NOT_SUPPORTED                        -44
+#define MA_CONNECTION_RESET                            -45
+#define MA_ALREADY_CONNECTED                           -46
+#define MA_NOT_CONNECTED                               -47
+#define MA_CONNECTION_REFUSED                          -48
+#define MA_NO_HOST                                     -49
+#define MA_IN_PROGRESS                                 -50
+#define MA_CANCELLED                                   -51
+#define MA_MEMORY_ALREADY_MAPPED                       -52
+#define MA_AT_END                                      -53
 
 /* General miniaudio-specific errors. */
 #define MA_FORMAT_NOT_SUPPORTED                        -100
@@ -40178,7 +40222,7 @@ static ma_result ma_decoder__full_decode_and_uninit(ma_decoder* pDecoder, ma_dec
 
             if ((newDataCapInFrames * bpf) > MA_SIZE_MAX) {
                 ma__free_from_callbacks(pPCMFramesOut, &pDecoder->allocationCallbacks);
-                return MA_TOO_LARGE;
+                return MA_TOO_BIG;
             }
 
 
