75e20452d97f160da02aace9eccbd2b8c441fec8
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2020-03-16 21:55:57 +0100

    Return proper result when fopen() fails.

--- miniaudio.h
+++ miniaudio.h
@@ -6813,6 +6813,7 @@ MA_API ma_result ma_fopen(FILE** ppFile, const char* pFilePath, const char* pOpe
         if (result == MA_SUCCESS) {
             return MA_ERROR;   /* Just a safety check to make sure we never ever return success when pFile == NULL. */
         }
+        return result;
     }
 #endif
 
