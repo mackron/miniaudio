b84d222c5e57b5bb5f05a377d84608caa1a90608
David Reid <mackron@gmail.com>
2021-07-04 15:27:09 +1000

    Remove miniaudio_dll.

--- extras/miniaudio_dll/miniaudio_dll.c
+++
@@ -1,2 +0,0 @@
-#include "miniaudio_dll.h"  /* For build options. */
-#include "../miniaudio_split/miniaudio.c"
--- extras/miniaudio_dll/miniaudio_dll.h
+++
@@ -1,9 +0,0 @@
-#ifndef miniaudio_dll_h
-#define miniaudio_dll_h
-
-/* ... set your build options here ... */
-
-#define MA_DLL
-#include "../miniaudio_split/miniaudio.h"
-
-#endif /* miniaudio_dll_h */
