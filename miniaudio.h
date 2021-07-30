4e2e371b26be6e6fb02de0b6037c9c6af634b16d
David Reid <mackron@gmail.com>
2021-07-31 09:25:02 +1000

    Fix a merge error.

--- miniaudio.h
+++ miniaudio.h
@@ -29485,7 +29485,6 @@ static ma_result ma_context__uninit_device_tracking__coreaudio(ma_context* pCont
             /* At this point there should be no tracked devices. If not there's an error somewhere. */
             if (g_ppTrackedDevices_CoreAudio != NULL) {
                 ma_log_postf(ma_context_get_log(pContext), MA_LOG_LEVEL_WARNING, "You have uninitialized all contexts while an associated device is still active.", MA_INVALID_OPERATION);
-                return MA_INVALID_OPERATION;
                 ma_spinlock_unlock(&g_DeviceTrackingInitLock_CoreAudio);
                 return MA_INVALID_OPERATION;
             }
