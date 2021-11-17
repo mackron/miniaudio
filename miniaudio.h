cf02c9e9a27dfde4569e8f108a07467f06a16084
David Reid <mackron@gmail.com>
2021-11-17 19:18:35 +1000

    WebAudio: Fix errors in strict mode.
    Public issue https://github.com/mackron/miniaudio/issues/363

--- miniaudio.h
+++ miniaudio.h
@@ -32300,7 +32300,7 @@ static ma_result ma_device_init_by_type__webaudio(ma_device* pDevice, const ma_d
         var isCapture  = $3;
         var pDevice    = $4;
 
-        if (typeof(miniaudio) === 'undefined') {
+        if (typeof(window.miniaudio) === 'undefined') {
             return -1;  /* Context not initialized. */
         }
 
@@ -32597,8 +32597,8 @@ static ma_result ma_context_init__webaudio(ma_context* pContext, const ma_contex
             return 0;   /* Web Audio not supported. */
         }
 
-        if (typeof(miniaudio) === 'undefined') {
-            miniaudio = {};
+        if (typeof(window.miniaudio) === 'undefined') {
+            window.miniaudio = {};
             miniaudio.devices = [];   /* Device cache for mapping devices to indexes for JavaScript/C interop. */
 
             miniaudio.track_device = function(device) {
@@ -69443,6 +69443,7 @@ REVISION HISTORY
 v0.10.43 - TBD
   - ALSA: Fix use of uninitialized variables
   - ALSA: Fix enumeration of devices that support both playback and capture.
+  - WebAudio: Fix errors in strict mode.
 
 v0.10.42 - 2021-08-22
   - Fix a possible deadlock when stopping devices.
