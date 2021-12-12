edbbb2abe75703b398223181c220a77c1f2894be
David Reid <mackron@gmail.com>
2021-12-12 14:40:45 +1000

    Standardize the return type on some functions.

--- miniaudio.h
+++ miniaudio.h
@@ -10016,7 +10016,7 @@ MA_API ma_engine* ma_sound_get_engine(const ma_sound* pSound);
 MA_API ma_data_source* ma_sound_get_data_source(const ma_sound* pSound);
 MA_API ma_result ma_sound_start(ma_sound* pSound);
 MA_API ma_result ma_sound_stop(ma_sound* pSound);
-MA_API ma_result ma_sound_set_volume(ma_sound* pSound, float volume);
+MA_API void ma_sound_set_volume(ma_sound* pSound, float volume);
 MA_API void ma_sound_set_pan(ma_sound* pSound, float pan);
 MA_API void ma_sound_set_pan_mode(ma_sound* pSound, ma_pan_mode panMode);
 MA_API void ma_sound_set_pitch(ma_sound* pSound, float pitch);
@@ -10070,7 +10070,7 @@ MA_API void ma_sound_group_uninit(ma_sound_group* pGroup);
 MA_API ma_engine* ma_sound_group_get_engine(const ma_sound_group* pGroup);
 MA_API ma_result ma_sound_group_start(ma_sound_group* pGroup);
 MA_API ma_result ma_sound_group_stop(ma_sound_group* pGroup);
-MA_API ma_result ma_sound_group_set_volume(ma_sound_group* pGroup, float volume);
+MA_API void ma_sound_group_set_volume(ma_sound_group* pGroup, float volume);
 MA_API void ma_sound_group_set_pan(ma_sound_group* pGroup, float pan);
 MA_API void ma_sound_group_set_pan_mode(ma_sound_group* pGroup, ma_pan_mode panMode);
 MA_API void ma_sound_group_set_pitch(ma_sound_group* pGroup, float pitch);
@@ -70643,16 +70643,14 @@ MA_API ma_result ma_sound_stop(ma_sound* pSound)
     return MA_SUCCESS;
 }
 
-MA_API ma_result ma_sound_set_volume(ma_sound* pSound, float volume)
+MA_API void ma_sound_set_volume(ma_sound* pSound, float volume)
 {
     if (pSound == NULL) {
-        return MA_INVALID_ARGS;
+        return;
     }
 
     /* The volume is controlled via the output bus. */
     ma_node_set_output_bus_volume(pSound, 0, volume);
-
-    return MA_SUCCESS;
 }
 
 MA_API void ma_sound_set_pan(ma_sound* pSound, float pan)
@@ -71212,9 +71210,9 @@ MA_API ma_result ma_sound_group_stop(ma_sound_group* pGroup)
     return ma_sound_stop(pGroup);
 }
 
-MA_API ma_result ma_sound_group_set_volume(ma_sound_group* pGroup, float volume)
+MA_API void ma_sound_group_set_volume(ma_sound_group* pGroup, float volume)
 {
-    return ma_sound_set_volume(pGroup, volume);
+    ma_sound_set_volume(pGroup, volume);
 }
 
 MA_API void ma_sound_group_set_pan(ma_sound_group* pGroup, float pan)
