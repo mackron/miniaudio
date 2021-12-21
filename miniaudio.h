f0675354747c02b45e0366ece5b8b1dcd0ab5ab5
kgdev <kg@unity3d.com>
2021-12-21 13:45:59 +0800

    remove unnecessary space after "defined"

--- miniaudio.h
+++ miniaudio.h
@@ -3675,7 +3675,7 @@ extern "C" {
     #endif
 #endif
 
-#if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__)) || defined(_M_X64) || defined(__ia64) || defined (_M_IA64) || defined(__aarch64__) || defined(_M_ARM64) || defined(__powerpc64__)
+#if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__)) || defined(_M_X64) || defined(__ia64) || defined(_M_IA64) || defined(__aarch64__) || defined(_M_ARM64) || defined(__powerpc64__)
     #define MA_SIZEOF_PTR   8
 #else
     #define MA_SIZEOF_PTR   4
