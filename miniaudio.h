0433ac9eca670f4afcbd837c07331e26b483c270
David Reid <mackron@gmail.com>
2023-03-21 09:25:26 +1000

    Fix some warnings.

--- miniaudio.h
+++ miniaudio.h
@@ -3799,7 +3799,7 @@ typedef double      ma_double;
 
 typedef void* ma_handle;
 typedef void* ma_ptr;
-typedef void (* ma_proc)(void);
+typedef void* ma_proc;
 
 #if defined(_MSC_VER) && !defined(_WCHAR_T_DEFINED)
 typedef ma_uint16 wchar_t;
@@ -11773,7 +11773,7 @@ static MA_INLINE void ma_sleep(ma_uint32 milliseconds)
 }
 #endif
 
-static MA_INLINE void ma_yield()
+static MA_INLINE void ma_yield(void)
 {
 #if defined(__i386) || defined(_M_IX86) || defined(__x86_64__) || defined(_M_X64)
     /* x86/x64 */
@@ -11808,7 +11808,7 @@ static MA_INLINE void ma_yield()
 #define MA_MM_DENORMALS_ZERO_MASK   0x0040
 #define MA_MM_FLUSH_ZERO_MASK       0x8000
 
-static MA_INLINE unsigned int ma_disable_denormals()
+static MA_INLINE unsigned int ma_disable_denormals(void)
 {
     unsigned int prevState;
 
@@ -41246,7 +41246,7 @@ MA_API ma_result ma_context_uninit(ma_context* pContext)
     return MA_SUCCESS;
 }
 
-MA_API size_t ma_context_sizeof()
+MA_API size_t ma_context_sizeof(void)
 {
     return sizeof(ma_context);
 }
@@ -53646,7 +53646,7 @@ MA_API ma_result ma_channel_converter_get_output_channel_map(const ma_channel_co
 Data Conversion
 
 **************************************************************************************************************************************************************/
-MA_API ma_data_converter_config ma_data_converter_config_init_default()
+MA_API ma_data_converter_config ma_data_converter_config_init_default(void)
 {
     ma_data_converter_config config;
     MA_ZERO_OBJECT(&config);
