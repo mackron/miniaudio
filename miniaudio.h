73e9955b1dab1f131d2f436c33b3e9a288ab8912
David Reid <mackron@gmail.com>
2023-11-30 09:38:01 +1000

    Fix an error with multiple defined macros.
    Public issue https://github.com/mackron/miniaudio/issues/780

--- miniaudio.h
+++ miniaudio.h
@@ -12313,15 +12313,6 @@ static MA_INLINE void ma_zero_memory_default(void* p, size_t sz)
 
 #define MA_ZERO_OBJECT(p)               MA_ZERO_MEMORY((p), sizeof(*(p)))
 
-#define ma_countof(x)                   (sizeof(x) / sizeof(x[0]))
-#define ma_max(x, y)                    (((x) > (y)) ? (x) : (y))
-#define ma_min(x, y)                    (((x) < (y)) ? (x) : (y))
-#define ma_abs(x)                       (((x) > 0) ? (x) : -(x))
-#define ma_clamp(x, lo, hi)             (ma_max(lo, ma_min(x, hi)))
-#define ma_offset_ptr(p, offset)        (((ma_uint8*)(p)) + (offset))
-#define ma_align(x, a)                  (((x) + ((a)-1)) & ~((a)-1))
-#define ma_align_64(x)                  ma_align(x, 8)
-
 #define ma_buffer_frame_capacity(buffer, channels, format) (sizeof(buffer) / ma_get_bytes_per_sample(format) / (channels))
 
 static MA_INLINE double ma_sind(double x)
