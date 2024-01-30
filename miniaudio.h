aa57d052da2f63e07ada293e18f43a19fc9b9e23
Chris Genova <raven.cgg@gmail.com>
2024-01-29 19:26:59 -0500

    Fix out of range check in ma_default_vfs_seek__win32

--- miniaudio.h
+++ miniaudio.h
@@ -59221,7 +59221,7 @@ static ma_result ma_default_vfs_seek__win32(ma_vfs* pVFS, ma_vfs_file file, ma_i
         result = ma_SetFilePointerEx((HANDLE)file, liDistanceToMove, NULL, dwMoveMethod);
     } else if (ma_SetFilePointer != NULL) {
         /* No SetFilePointerEx() so restrict to 31 bits. */
-        if (origin > 0x7FFFFFFF) {
+        if (offset > 0x7FFFFFFF) {
             return MA_OUT_OF_RANGE;
         }
 
